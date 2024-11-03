02ad03d9259269e2dcc4ea0ba4f51ec67bd63a86
Nico9874 <155961795+Nico9874@users.noreply.github.com>
2024-11-03 10:51:05 +0100

    Update Extraction.py
    se agrego la columna age_classification, age_classification, age_at_registration, time_registered y continent

--- Extraction.py
+++ Extraction.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import requests
 import sqlite3
+import math
 # Set root path of API
 api_root = 'https://randomuser.me/'
 
@@ -178,28 +179,103 @@ class Database:
                 print(result[0])
                 self.cursor.execute(result[0])
         self.conn.commit()
+
+#nico le agrega la columna generation, age_classification
+
     def insert_user(self, user):
+        # Clasificar generación basada en la edad
+        if user.age >= 77:
+            generation = 'Silent Generation'
+        elif user.age >= 59:
+            generation = 'Baby Boomer'
+        elif user.age >= 43:
+            generation = 'Generation X'
+        elif user.age >= 27:
+            generation = 'Millennials'
+        elif user.age >= 11:
+            generation = 'Generation Z'
+        else:
+            generation = 'Generation Alpha'
+
+        # Clasificar edad en intervalos
+        if user.age <= 10:
+            age_classification = 'Infancia 0-10'
+        elif user.age <= 20:
+            age_classification = 'Adolescencia 11-20'
+        elif user.age <= 30:
+            age_classification = 'Adultos jóvenes 21-30'
+        elif user.age <= 40:
+            age_classification = 'Adultos 31-40'
+        elif user.age <= 50:
+            age_classification = 'Mediana edad 41-50'
+        elif user.age <= 60:
+            age_classification = 'Adultos mayores 51-60'
+        elif user.age <= 70:
+            age_classification = 'Pre-senior 61-70'
+        else:
+            age_classification = 'Senior 71+'
+
+        # Calcular la edad al momento del registro
+        age_at_registration = math.floor((user.date_of_registration - user.dob).days / 365.25)
+
+        # Calcular el tiempo registrado en días
+        time_registered = math.floor((datetime.now() - user.date_of_registration).days / 365.25)
+
         self.cursor.execute(
             """
-            INSERT OR REPLACE INTO users (user_id, first_name, last_name, gender, email, phone, dob, date_of_registration, profile_picture)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+            INSERT OR REPLACE INTO users (user_id, first_name, last_name, gender, email, phone, dob, age, date_of_registration, profile_picture, generation, age_classification, age_at_registration, time_registered)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
             """,
-            (user.user_id, user.first_name, user.last_name, user.gender, user.email, user.phone, user.dob, user.date_of_registration, user.profile_picture)
+            (user.user_id, user.first_name, user.last_name, user.gender, user.email, user.phone, user.dob, user.age, user.date_of_registration, user.profile_picture, user.generation, user.age_classification, user.age_at_registration, user.time_registered)
         )
-        self.conn.commit()
+        self.conn.commit() #fin de la creacion de la columna en user
+
+
+#nico le agrega la columna por continente
 
     def insert_location(self, location):
+        # Diccionario de mapeo de país a continente
+        continent_mapping = {
+            'Brazil': 'Latam',
+            'Netherlands': 'Europa',
+            'India': 'Asia',
+            'France': 'Europa',
+            'Spain': 'Europa',
+            'Ukraine': 'Europa',
+            'Denmark': 'Europa',
+            'Ireland': 'Europa',
+            'New Zealand': 'Oceanía',
+            'Norway': 'Europa',
+            'United States': 'Norteamerica',
+            'United Kingdom': 'Europa',
+            'Canada': 'Norteamerica',
+            'Serbia': 'Europa',
+            'Mexico': 'Latam',
+            'Iran': 'Asia',
+            'Switzerland': 'Europa',
+            'Australia': 'Oceanía',
+            'Germany': 'Europa',
+            'Finland': 'Europa',
+            'Turkey': 'Asia Occidental'
+        }
+
+        # Obtener continente basado en el país
+        continent = continent_mapping.get(location.country, None)
+
         # Convert coordinates list to a JSON string
-        coordinates_json = location.coordinates[0]+ "," + location.coordinates[1]
+        coordinates_json = location.coordinates[0] + "," + location.coordinates[1]
+        
         self.cursor.execute(
             """
-            INSERT INTO locations (user_id, city, coordinates, country, postcode, state, street_name, street_number, timezone)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+            INSERT INTO locations (user_id, city, coordinates, country, postcode, state, street_name, street_number, timezone, continent)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
             """,
-            (location.user_id, location.city, coordinates_json, location.country, location.postcode, location.state, location.street_name, location.street_number, location.timezone)
+            (location.user_id, location.city, coordinates_json, location.country, location.postcode, location.state, location.street_name, location.street_number, location.timezone, location.continent)
         )
-        self.conn.commit()
+        self.conn.commit()# fin de la modificacón de agregado de columna en location
 
+    
+    
     def close(self):
         self.cursor.close()
         self.conn.close()
